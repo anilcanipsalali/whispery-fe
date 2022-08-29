365b408e7f943262fea39b757de73b5d2ec909f0
anilcanipsalali <anilcan.ipsalali@gmail.com>
2022-08-29 14:37:07 +0300

    DropdownMenu.js updated

--- src/pages/dashboard/AppBar/DropdownMenu.js
+++ src/pages/dashboard/AppBar/DropdownMenu.js
@@ -4,16 +4,23 @@ import MenuItem from "@mui/material/MenuItem";
 import { IconButton } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { logout } from "../../../utils/auth";
+import { getActions } from '../../../store/actions/roomActions';
+import { connect } from 'react-redux';
 
-export default function BasicMenu() {
+const BasicMenu = ({ audioOnly, setAudioOnly }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
+  
   const handleMenuClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleAudioOnlyChange = () => {
+    setAudioOnly(!audioOnly);
   };
 
   return (
@@ -31,7 +38,24 @@ export default function BasicMenu() {
         }}
       >
         <MenuItem onClick={logout}>Logout</MenuItem>
+        <MenuItem onClick={handleAudioOnlyChange}>
+          {audioOnly ? "Audio Only Enabled" : "Audio Only Disabled"}
+        </MenuItem>
       </Menu>
     </div>
   );
-}
+};
+
+const mapStoreStateToProps = ({ room }) => {
+  return {
+    ...room,
+  };
+};
+
+const mapActionsToProps = (dispatch) => {
+  return {
+    ...getActions(dispatch),
+  };
+};
+
+export default connect(mapStoreStateToProps, mapActionsToProps)(BasicMenu);
